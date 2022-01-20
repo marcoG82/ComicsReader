0b3e0362e80148ae1f5479dc0a9571c546e4a8cc
Marco Giustozzi <32507837+marcoG82@users.noreply.github.com>
2022-01-20 15:57:55 +0100

    first commit

---
+++ ComicsReader/ComicsReader/ComicsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicsReaderApp.swift
+//  ComicsReader
+//
+//  Created by Marco Giustozzi on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct ComicsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
